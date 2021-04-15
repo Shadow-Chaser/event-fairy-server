09179a8e85cfc558a315d8c69313748972642c1c
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-04-16 02:41:49 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# complete-website-server-Shadow-Chaser
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(cors());
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8cui.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const servicesCollection = client.db(`${process.env.DB_NAME}`).collection("services");
+
+  app.post('/addService', (req, res)=>{
+    const file = req.files.file;
+    const title = req.body.tile;
+    const description= req.body.description;
+    const price= req.body.price;
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
+
+    var image = {
+        contentType: file.mimetype,
+        size: file.size,
+        img: Buffer.from(encImg, 'base64')
+    };
+
+    servicesCollection.insertOne({ title, description, price, image })
+    .then(result => {
+        console.log('inserted count', result.insertedCount);
+        res.send(result.insertedCount > 0)
+    })
+  })
+
+  app.get('/services', (req, res) => {
+    servicesCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+});
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
