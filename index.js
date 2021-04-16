9bcd1a65b5c0b2c2a68d24742bf945655eeb30de
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-04-16 14:13:39 +0600

    services and review API

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const servicesCollection = client.db(`${process.env.DB_NAME}`).collection("services");
+  const reviewsCollection = client.db(`${process.env.DB_NAME}`).collection("reviews");
 
   app.post('/addService', (req, res)=>{
     const file = req.files.file;
-    const title = req.body.tile;
+    const title = req.body.title;
     const description= req.body.description;
     const price= req.body.price;
     const newImg = file.data;
     const encImg = newImg.toString('base64');
+    console.log(title);
 
     var image = {
         contentType: file.mimetype,
@@ -39,12 +41,42 @@ client.connect(err => {
     })
   })
 
+  app.post('/addReview', (req, res) => {
+    const file = req.files.file;
+    const name = req.body.name;
+    const review = req.body.review;
+    const address = req.body.address;
+    const newImg = file.data;
+    const encImg = newImg.toString('base64');
+    // console.log(title);
+
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, 'base64')
+    };
+
+    reviewsCollection.insertOne({ name, review, address, image })
+      .then(result => {
+        console.log('inserted count', result.insertedCount);
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+
   app.get('/services', (req, res) => {
     servicesCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
-});
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  app.get('/reviews', (req, res) => {
+    reviewsCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
 
 });
 
